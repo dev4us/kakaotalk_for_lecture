32201721b4643737e91c8e5568e4a18154bd7fc2
dev4us <bronxofsoul@gmail.com>
2019-12-08 01:22:26 +0900

    [BE] create graphql TypeDefs

--- backend/src/api/Hello/sayHello/sayHello.resolvers.ts
+++
@@ -1,7 +0,0 @@
-const resolvers = {
-  Query: {
-    sayHello: () => "Hey! Welcome"
-  }
-};
-
-export default resolvers;
--- backend/src/types/graphql.d.ts
+++ backend/src/types/graphql.d.ts
@@ -1,7 +1,24 @@
-export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello: String!\n}\n"];
+export const typeDefs = ["type Message {\n  id: Int!\n  text: String\n  sendTo: User\n  sendBy: User\n  createdAt: String!\n}\n\ntype Query {\n  message: Message\n  user: User\n}\n\ntype User {\n  id: Int!\n  email: String!\n  password: String!\n  username: String!\n  profilePhoto: String\n  friends: [User]\n}\n"];
 /* tslint:disable */
 
 export interface Query {
-  sayBye: string;
-  sayHello: string;
+  message: Message | null;
+  user: User | null;
+}
+
+export interface Message {
+  id: number;
+  text: string | null;
+  sendTo: User | null;
+  sendBy: User | null;
+  createdAt: string;
+}
+
+export interface User {
+  id: number;
+  email: string;
+  password: string;
+  username: string;
+  profilePhoto: string | null;
+  friends: Array<User> | null;
 }
