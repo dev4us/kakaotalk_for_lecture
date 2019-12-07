5cfc91fa24f41e18cdd926675df2ce7e94efc363
dev4us <bronxofsoul@gmail.com>
2019-12-08 01:07:19 +0900

    [BE] Create first Entity & check to db

---
+++ backend/src/entities/User.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany
+} from "typeorm";
+
+@Entity()
+class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "text", nullable: false })
+  email: string;
+
+  @Column({ type: "text", nullable: false })
+  password: string;
+
+  @Column({ type: "text", nullable: false })
+  username: string;
+
+  @Column({ type: "text", nullable: false })
+  profilePhoto: string;
+
+  @OneToMany(
+    type => User,
+    user => user.friends
+  )
+  friends: User[];
+}
+
+export default User;
